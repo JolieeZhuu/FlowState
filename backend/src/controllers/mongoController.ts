c442fada1ee8f5f39da0b51f1cbc36422e9b3014
Derek <derekwork36@gmail.com>
2025-10-26 01:54:43 -0400

    fixed delete

--- backend/src/controllers/mongoController.ts
+++ backend/src/controllers/mongoController.ts
@@ -16,19 +16,20 @@ export async function getUserTasks(req: AuthRequest, res: Response){
 export async function getTask(req : AuthRequest, res: Response){
     try {
         const taskId = req.query.id
-        const task = await Task.findById(taskId);
+        const task = await Task.findOne({id: taskId});
         if (!task) {
             return res.status(404).json({ message: 'Task not found' });
         }
+        console.log("Got Task")
         res.json(task);
     }
     catch (error) {
+        console.error('Find task error:', error); 
         res.status(500).json({ message: 'Error fetching task', error });
     }
 }
 
 export async function createTask(req: AuthRequest, res: Response){
-    console.log(req.body)
     try{
         const task = new Task({
             id: req.body.id,
@@ -57,7 +58,7 @@ export async function createTask(req: AuthRequest, res: Response){
 export async function editTask(req: AuthRequest, res: Response){
     try{
         const task = await Task.findOneAndUpdate(
-        { _id: req.params.id, userId: req.user?._id },
+        { id: req.body.id, userId: req.user?._id },
         {
             id: req.body.id,
             title: req.body.title,
@@ -78,10 +79,11 @@ export async function editTask(req: AuthRequest, res: Response){
         if (!task) {
             return res.status(404).json({ message: 'Task not found' });
         }
-        
+        console.log("Edited Task")
         res.json(task);
     }
     catch(error){
+        console.error('Edit task error:', error); 
         res.status(400).json({ message: 'Error updating task', error });
     }
 }
@@ -89,13 +91,15 @@ export async function editTask(req: AuthRequest, res: Response){
 export async function deleteTask(req: AuthRequest, res: Response){
     try{
         const taskId = req.query.id;
-        const task = await Task.findByIdAndDelete(taskId);
+        const task = await Task.findOneAndDelete({id: taskId});
         if (!task) {
             return res.status(404).json({ message: 'Task not found' });
         }
+        console.log("Deleted Task")
         res.json({ message: 'Task deleted successfully' });
     } 
     catch(error){
+        console.error('Delete task error:', error); 
         res.status(500).json({ message: 'Error deleting task', error });
     }
 }
