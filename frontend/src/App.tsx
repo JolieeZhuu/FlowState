0481a77c0b6ce41ca190ea4e819a67a3de48c348
Derek <derekwork36@gmail.com>
2025-10-25 20:03:16 -0400

    Googleauth (#5)
    * made google auth
    
    * small chage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import TabbedApp from './components/Tabs'
+import Login from './login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-        <TabbedApp/>
+        {/*<TabbedApp/>*/}
+        <Login/>
     </div>
   )
 }
